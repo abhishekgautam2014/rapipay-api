fd98c82fc65bf0f83ab0a75ffd40ebf77f1ad8f4
Antialis <antialiasventures@gmail.com>
2021-10-23 16:14:30 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c6232fe082252555b3fc50065d6fb2db'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2ff0eab1-0fee-4fdd-a0b4-d709ef8eebae'
+};
